55c8f0e678a0fa746617b0d5da6b2a42aecfb40d
siriluk <nastia_mai@hotmail.com>
2024-03-24 17:44:42 +0700

    report metric

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.scss";
-import type { AppProps } from "next/app";
+import type { AppProps, NextWebVitalsMetric } from "next/app";
 import { wrapper } from "./store";
 import { Provider } from "react-redux";
 import Layout from "./components/layout";
@@ -22,7 +22,33 @@ const messages: any = {
   en,
   th
 }
-console.log(messages)
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.log(metric)
+
+  // const body = JSON.stringify(metric)
+  // const url = 'https://example.com/analytics'
+
+  // if (navigator.sendBeacon) {
+  //   navigator.sendBeacon(url, body)
+  // } else {
+  //   fetch(url, {body, method : 'POST', keepalive: true})
+  // }
+}
+
+// Example Google Tag manager to store the browser metrics
+// export function reportWebVitals({id, name, label, value}:any) {
+//   window.gtag('event', name, {
+//     event_category:
+//     label === 'web-vital'? 'Web Vitals': 'Next.js custom metric',
+//     value: Math.round(name === 'CLS'? value*100 : value),
+//     event_label: id,
+//     non_interaction: true
+//   })
+// }
+
+
+
 export default function CookBook({ Component, pageProps }: AppProps) {
   const { store } = wrapper.useWrappedStore(pageProps);
 
@@ -30,7 +56,7 @@ export default function CookBook({ Component, pageProps }: AppProps) {
     const authFromStorage = getFromStorageByKey(LocalStorageKeys.LOGIN)
 
     if (authFromStorage) {
-      console.log(authFromStorage)
+      // console.log(authFromStorage)
       store.dispatch(changeAuthState(authFromStorage));
     }
 
@@ -43,7 +69,7 @@ export default function CookBook({ Component, pageProps }: AppProps) {
     locale,
     messages,
   });
-  console.log(intl)
+
   //const localeToString = locale as string;
   // console.log(localeToString)
 
